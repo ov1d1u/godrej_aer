befe5229a2c64338cc6e678e424840a584832093
Ovidiu Nitan <nitanovidiu@gmail.com>
2026-01-26 13:45:24 +0200

    Fix possible deadlock in get_device_status()

--- custom_components/godrej_aer/godrej/godrej.py
+++ custom_components/godrej_aer/godrej/godrej.py
@@ -45,8 +45,11 @@ class SmartMatic:
         self.mac = mac
 
     async def connect(self) -> bool:
+        _LOGGER.debug("Trying to connect to device %s...", self.mac)
+        
         async with self._connect_lock:
             if self.client and self.client.is_connected:
+                _LOGGER.debug("Already connected to %s", self.mac)
                 return True
 
             device = async_ble_device_from_address(
@@ -66,16 +69,17 @@ class SmartMatic:
 
             await asyncio.sleep(2.0)  # give some time for service discovery
 
-            _LOGGER.debug("Connected to %s", self.mac)
+            _LOGGER.debug("Connected to %s, discovering services...", self.mac)
 
             services = self.client.services
             if not MAIN_SVC in [service.uuid for service in services]:
-                self.client.disconnect()
+                await self.client.disconnect()
                 raise InvalidDeviceError("Device does not look right")
 
             self.eventbus.send(DEVICE_CONNECT, self)
 
             try:
+                _LOGGER.debug("Subscribing to notifications on %s...", NOTIFY_CHAR)
                 await self.client.start_notify(
                     NOTIFY_CHAR,
                     self._notification_handler
@@ -103,35 +107,44 @@ class SmartMatic:
             _LOGGER.debug("Disconnecting from %s...", self.mac)
             await self.client.disconnect()
             return True
+        
+        _LOGGER.debug("%s already disconnected.", self.mac)
+        return False
 
     async def delayed_disconnect(self):
         async def _delayed_disconnect():
             if not self.client.is_connected:
+                _LOGGER.debug("%s already disconnected, skipping delayed disconnect.", self.mac)
                 return
 
             try:
+                _LOGGER.debug("Waiting %ss before disconnecting from %s...", DISCONNECT_DELAY, self.mac)
                 await asyncio.sleep(DISCONNECT_DELAY)
                 await self.disconnect()
             except Exception as e:
                 _LOGGER.debug("Failed to disconnect. Error: %s", e)
 
+        _LOGGER.debug("Scheduling delayed disconnect from %s...", self.mac)
         loop = asyncio.get_running_loop()
         if self._disconnect_task is not None:
             self._disconnect_task.cancel()
         self._disconnect_task = loop.create_task(_delayed_disconnect())
 
     async def get_device_status(self):
-        await self._ensure_connected()
-
+        _LOGGER.debug("Getting device status from %s...", self.mac)
+        
         # Reset the event before waiting for new status
         self._device_status_event.clear()
 
+        _LOGGER.debug("Writing to %s on %s...", WRITE_CHAR, self.mac)
         await self.client.write_gatt_char(
             WRITE_CHAR,
             bytes.fromhex("bf626d54186b626d4e189dff")
         )
         
         try:
+            _LOGGER.debug("Waiting for device status from %s...", self.mac)
+            
             await asyncio.wait_for(
                 self._device_status_event.wait(),
                 STATUS_TIMEOUT
@@ -149,28 +162,34 @@ class SmartMatic:
         await self.delayed_disconnect()
 
     async def trigger(self):
+        _LOGGER.debug("Triggering device %s...", self.mac)
         await self._ensure_connected()
 
+        _LOGGER.debug("Writing to %s on %s...", WRITE_CHAR, self.mac)
         await self.client.write_gatt_char(
             WRITE_CHAR,
             bytes.fromhex("bf626d541868626d4e189a62724900ff")
         )
+        _LOGGER.debug("Write complete.")
 
         await self.delayed_disconnect()
 
     async def _ensure_connected(self):
-        async def wait_for_connected():
-            while not self.client or not self.client.is_connected:
-                try:
-                    await self.connect()
-                except:
-                    await asyncio.sleep(1)
-                    continue
+        if not self.client or not self.client.is_connected:
+            _LOGGER.debug("Not connected, connecting to %s...", self.mac)
+            await self.connect()
+        # async def wait_for_connected():
+        #     while not self.client or not self.client.is_connected:
+        #         try:
+        #             await self.connect()
+        #         except:
+        #             await asyncio.sleep(1)
+        #             continue
 
-        try:
-            await asyncio.wait_for(wait_for_connected(), CONNECTION_TIMEOUT)
-        except asyncio.TimeoutError as exc:
-            raise NotConnectedError("Connection timeout") from exc
+        # try:
+        #     await asyncio.wait_for(wait_for_connected(), CONNECTION_TIMEOUT)
+        # except asyncio.TimeoutError as exc:
+        #     raise NotConnectedError("Connection timeout") from exc
 
     async def _notification_handler(self, sender, data):
         if sender.uuid.lower() == NOTIFY_CHAR.lower():
